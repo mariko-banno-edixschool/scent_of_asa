5d8c999db1a6ec5bfe719aa3fb3c206d2ce94666
LAPTOP-K3PN8E57\academia <202511_aw_banno@x-ide.sakura.ne.jp>
2026-03-27 14:05:04 +0900

    初回コミット

---
+++ src/main/java/com/Edo_perfume/ScentOfASA/ScentOfAsaApplication.java
@@ -0,0 +1,13 @@
+package com.Edo_perfume.ScentOfASA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScentOfAsaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScentOfAsaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Edo_perfume/ScentOfASA/ScentOfAsaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Edo_perfume.ScentOfASA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScentOfAsaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
